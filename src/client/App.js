f3d447098d787996159b5ce3eaced9133d77df52
kiran <kiran@neostack.com>
2019-07-06 13:07:42 +0530

    subscriptions

--- src/client/App.js
+++ src/client/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
+import { SubscriptionClient } from "subscriptions-transport-ws";
 import "./App.css";
-import { Provider, createClient } from "urql";
+import {
+  // cacheExchange,
+  // debugExchange,
+  // fetchExchange,
+  Provider,
+  createClient,
+  defaultExchanges,
+  subscriptionExchange
+} from "urql";
 import Chat from "./Chat";
 
+const subscriptionClient = new SubscriptionClient(
+  "ws://localhost:8000/graphql",
+  {}
+);
+
 const client = createClient({
-  url: "http://localhost:8000/graphql"
+  url: "http://localhost:8000/graphql",
+  exchanges: [
+    ...defaultExchanges,
+    subscriptionExchange({
+      forwardSubscription: operation => subscriptionClient.request(operation)
+    })
+  ]
 });
 
 class App extends Component {
--- src/client/Messages.js
+++ src/client/Messages.js
@@ -1,12 +1,20 @@
 import React from "react";
 import gql from "graphql-tag";
-import { useQuery } from "urql";
+import { useQuery, useSubscription } from "urql";
 
 export default function Messages() {
-  const [res] = useQuery({
+  const [res, fetchMessages] = useQuery({
     query: messagesQuery
   });
 
+  const [subscriptionResult] = useSubscription(
+    { query: messageSubscription },
+    handleSubscription
+  );
+
+  function handleSubscription(messages, response) {
+    fetchMessages({ requestPolicy: "network-only" });
+  }
   if (res.fetching) {
     return "Loading...";
   } else if (res.error) {
@@ -24,24 +32,6 @@ export default function Messages() {
   );
 }
 
-// export class Messages extends React.Component {
-//   componentDidMount() {
-//     this.props.subscribeToNewMessage();
-//   }
-//   render() {
-//     console.log(this.props);
-//     const { data: { messages, loading } } = this.props;
-//     if (loading) {
-//       return <div>Loading messages ...</div>;
-//     }
-//     return (
-//       <div>
-//         {messages.map(message => <div key={message.id}>{message.text}</div>)}
-//       </div>
-//     );
-//   }
-// }
-
 const messagesQuery = gql`
   {
     messages {
@@ -51,7 +41,7 @@ const messagesQuery = gql`
   }
 `;
 
-const messageSubscribe = gql`
+const messageSubscription = gql`
   subscription {
     onNewMessage {
       id
@@ -59,29 +49,3 @@ const messageSubscribe = gql`
     }
   }
 `;
-
-// export default graphql(messagesQuery, {
-//   props: props =>
-//     Object.assign({}, props, {
-//       subscribeToNewMessage: params => {
-//         console.log(props);
-//         return props.data.subscribeToMore({
-//           document: messageSubscribe,
-//           updateQuery: (prev, { subscriptionData }) => {
-//             console.log("subscribed data", subscriptionData);
-//             if (!subscriptionData.data) {
-//               return prev;
-//             }
-//             const newMessage = subscriptionData.data.onNewMessage;
-//             console.log(newMessage, prev.messages);
-//             if (prev.messages.find(message => message.id === newMessage.id)) {
-//               return prev;
-//             }
-//             return Object.assign({}, prev, {
-//               messages: [...prev.messages, newMessage]
-//             });
-//           }
-//         });
-//       }
-//     })
-// })(Messages);
