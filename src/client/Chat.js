f126595afb8cf98a71a280978eadd2a59c55561c
kiran <kiran@neostack.com>
2019-07-06 12:36:29 +0530

    convert messages list to urql

--- src/client/Chat.js
+++ src/client/Chat.js
@@ -1,13 +1,12 @@
 import React from "react";
-// import Messages from "./Messages";
+import Messages from "./Messages";
 // import MessageInput from "./MessageInput";
-import Users from "./Users";
 
 export default class Chat extends React.Component {
   render() {
     return (
       <div>
-        <Users />
+        <Messages />
       </div>
     );
   }
--- src/client/Messages.js
+++ src/client/Messages.js
@@ -1,24 +1,46 @@
 import React from "react";
 import gql from "graphql-tag";
-import { graphql } from "react-apollo";
+import { useQuery } from "urql";
 
-export class Messages extends React.Component {
-  componentDidMount() {
-    this.props.subscribeToNewMessage();
+export default function Messages() {
+  const [res] = useQuery({
+    query: messagesQuery
+  });
+
+  if (res.fetching) {
+    return "Loading...";
+  } else if (res.error) {
+    return "Oh no!";
+  } else if (!res.data.messages) {
+    return null;
   }
-  render() {
-    console.log(this.props);
-    const { data: { messages, loading } } = this.props;
-    if (loading) {
-      return <div>Loading messages ...</div>;
-    }
-    return (
-      <div>
-        {messages.map(message => <div key={message.id}>{message.text}</div>)}
-      </div>
-    );
-  }
+
+  return (
+    <ul>
+      {res.data.messages.map(({ id, text }) => (
+        <li key={id}>{text}</li>
+      ))}
+    </ul>
+  );
 }
+
+// export class Messages extends React.Component {
+//   componentDidMount() {
+//     this.props.subscribeToNewMessage();
+//   }
+//   render() {
+//     console.log(this.props);
+//     const { data: { messages, loading } } = this.props;
+//     if (loading) {
+//       return <div>Loading messages ...</div>;
+//     }
+//     return (
+//       <div>
+//         {messages.map(message => <div key={message.id}>{message.text}</div>)}
+//       </div>
+//     );
+//   }
+// }
 
 const messagesQuery = gql`
   {
@@ -38,28 +60,28 @@ const messageSubscribe = gql`
   }
 `;
 
-export default graphql(messagesQuery, {
-  props: props =>
-    Object.assign({}, props, {
-      subscribeToNewMessage: params => {
-        console.log(props);
-        return props.data.subscribeToMore({
-          document: messageSubscribe,
-          updateQuery: (prev, { subscriptionData }) => {
-            console.log("subscribed data", subscriptionData);
-            if (!subscriptionData.data) {
-              return prev;
-            }
-            const newMessage = subscriptionData.data.onNewMessage;
-            console.log(newMessage, prev.messages);
-            if (prev.messages.find(message => message.id === newMessage.id)) {
-              return prev;
-            }
-            return Object.assign({}, prev, {
-              messages: [...prev.messages, newMessage]
-            });
-          }
-        });
-      }
-    })
-})(Messages);
+// export default graphql(messagesQuery, {
+//   props: props =>
+//     Object.assign({}, props, {
+//       subscribeToNewMessage: params => {
+//         console.log(props);
+//         return props.data.subscribeToMore({
+//           document: messageSubscribe,
+//           updateQuery: (prev, { subscriptionData }) => {
+//             console.log("subscribed data", subscriptionData);
+//             if (!subscriptionData.data) {
+//               return prev;
+//             }
+//             const newMessage = subscriptionData.data.onNewMessage;
+//             console.log(newMessage, prev.messages);
+//             if (prev.messages.find(message => message.id === newMessage.id)) {
+//               return prev;
+//             }
+//             return Object.assign({}, prev, {
+//               messages: [...prev.messages, newMessage]
+//             });
+//           }
+//         });
+//       }
+//     })
+// })(Messages);
